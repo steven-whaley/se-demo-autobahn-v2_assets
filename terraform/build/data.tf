d793366730c1d950f5bd738144427e4c178b3d9c
Steven Whaley <steven.whaley@hashicorp.com>
2024-11-19 16:38:49 -0800

    remove local state data source

--- terraform/build/data.tf
+++ terraform/build/data.tf
@@ -1,11 +1,3 @@
-data "terraform_remote_state" "setup" {
-  backend = "local"
-
-  config = {
-    path = "../setup/terraform.tfstate"
-  }
-}
-
 data "aws_availability_zones" "available" {
   state = "available"
 }
