e000b510d8e6d0caf2143550b702d437f3a0df72
Steven Whaley <steven.whaley@hashicorp.com>
2024-11-21 18:21:28 -0800

    Added AMI id output

--- terraform/build/outputs.tf
+++ terraform/build/outputs.tf
@@ -2,3 +2,8 @@ output "webapp_url" {
   description = "The URL where you can reach the web app"
   value       = "http://${aws_lb.webapp_lb.dns_name}"
 }
+
+output "ami_id" {
+  description = "The AMI of the image we created with packer"
+  value = data.hcp_packer_artifact.terramino_this_region.external_identifier
+}
